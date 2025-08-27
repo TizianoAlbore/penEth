34c9c85be72273894189357521bf26b89943d599
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2025-08-27 15:31:48 +0200

    revert redis stuff

--- server/app.js
+++ server/app.js
@@ -64,6 +64,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Redis
+const { connectRedis } = require('./config/redis');
+connectRedis();
+
 // Routes
 app.use("/api", jwksRouter);
 app.use("/api", authRouter);
